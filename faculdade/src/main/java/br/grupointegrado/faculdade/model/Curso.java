b3bf27019409396741ebaa5c0ac50dc45505a0b1
JeanGrendel <wersgaming@hotmail.com>
2024-11-23 16:21:39 -0300

    controller,dto,model,repository de curso

---
+++ faculdade/src/main/java/br/grupointegrado/faculdade/model/Curso.java
@@ -0,0 +1,55 @@
+package br.grupointegrado.faculdade.model;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "cursos")
+public class Curso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String nome;
+
+    @Column(unique = true)
+    private String codigo;
+
+    @Column
+    private int carga_horaria;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getCarga_horaria() {
+        return carga_horaria;
+    }
+
+    public void setCarga_horaria(int carga_horaria) {
+        this.carga_horaria = carga_horaria;
+    }
+}
